83d5875ceafe41faaaf8bb83eb6fd63a04462c7c
Mark Biek <support@janustech.net>
2016-08-25 08:59:04 -0600

    Moved data feeds to their own file

---
+++ src/client/app/feeds.js
@@ -0,0 +1,44 @@
+class EarthquakeFeeds {
+    constructor(props) {
+        this.feeds = {
+            hour: {
+                mag: {
+                    all: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson',
+                    m1: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson',
+                    m2: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson',
+                    m4: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_hour.geojson',
+                    sig: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson'
+                }
+            },
+            day: {
+                mag: {
+                    all: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson',
+                    m1: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson',
+                    m2: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson',
+                    m4: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson',
+                    sig: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson'
+                }
+            },
+            week: {
+                mag: {
+                    all: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson',
+                    m1: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson',
+                    m2: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson',
+                    m4: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson',
+                    sig: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson'
+                }
+            },
+            month: {
+                mag: {
+                    all: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson',
+                    m1: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson',
+                    m2: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson',
+                    m4: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson',
+                    sig: 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
+                }
+            }
+        };
+    }
+}
+
+export default EarthquakeFeeds;
